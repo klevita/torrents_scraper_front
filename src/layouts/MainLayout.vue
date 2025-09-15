b0e65d20d77e0e635c2032ab3994cc67f4adec25
Klementyev Vitaliy <kklevita92@gmail.com>
2025-09-15 13:21:52 +0300

    lab4 test

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,7 +2,7 @@
   <q-layout view="lHh Lpr lFf">
     <q-header elevated>
       <q-toolbar>
-        <q-toolbar-title> Quasar App </q-toolbar-title>
+        <q-toolbar-title> DevOps lab4 </q-toolbar-title>
 
         <div>Quasar v{{ $q.version }}</div>
       </q-toolbar>
